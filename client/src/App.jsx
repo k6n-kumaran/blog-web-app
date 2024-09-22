fc0859386b3cf6eebc7ecb5c85c9aa00abe225d6
gunakrn <gunakumaran01.gmail.com>
2024-09-22 17:22:52 +0530

    creating the header section using flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from './pages/About'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
 
 
 
@@ -13,6 +14,7 @@ function App() {
   return (
    <>
      <BrowserRouter>
+     <Header />
        <Routes>
          <Route path='/' element = {<Home />} />
          <Route path='/about' element = {<About />} />
